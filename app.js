bd0f4c73f07814f18094cf8f089adc3f4db38c3c
Alessio Porta <alessioporta@Air-di-Alessio.homenet.telecomitalia.it>
2020-10-12 15:23:58 +0200

    Add mongoDb and default collections

--- app.js
+++ app.js
@@ -1,22 +1,55 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js"); // modulo locale
+//const date = require(__dirname + "/date.js"); // modulo locale
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
-const items = [];
-const workItems = [];
-// NB: posso fare push su un array const, ma non posso fare una dichiarazione diretta 
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/", function(req, res) {
-	const day = date.getDate();
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
 
-	res.render('list', {listTitle: day, listItems: items});
+const itemSchema = {
+	name: {
+		type: String,
+		required: [true, "Specifie a name"]
+	}
+}
+
+const Item = mongoose.model("Item", itemSchema);
+
+const item1 = new Item ({
+	name: "Cosa1"
+})
+const item2 = new Item ({
+	name: "Cosa2"
+})
+const item3 = new Item ({
+	name: "Cosa3"
+})
+
+const defaultItems = [item1, item2, item3];
+
+app.get("/", function(req, res) {
+	//const day = date.getDate();
+	Item.find({}, function(err, docs) {
+		if(docs.length === 0){
+			//insert default data
+			Item.insertMany(defaultItems, function(err) {
+				if(err){
+					console.log(err);
+				}else {
+					console.log('Inserted default items!')
+				}
+			});
+			res.redirect("/");
+		}else{
+			res.render('list', {listTitle: "Today", listItems: docs});
+		}
+	});
 });
 
 app.post("/", function(req, res) {
