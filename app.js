a35c4364707162d9804777ff55f8e54fe95d0d3a
Alessio Porta <alessioporta@Air-di-Alessio.homenet.telecomitalia.it>
2020-10-13 15:57:16 +0200

    Add atlas cloud db

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+//mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://root:root@cluster0.tydfh.mongodb.net/todolistDB", {useNewUrlParser: true});
 
 const itemSchema = {
 	name: {
